b520243b9a67babbc1f36bbec4a0a6be479f3ad2
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-08 22:38:08 +0530

    fix bugs

--- Database/ClientDetails.js
+++ Database/ClientDetails.js
@@ -1,6 +1,6 @@
 module.exports = {
   host: process.env.POSTGRES_URL,
-  port: 5433,
+  port: 5432,
   user: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
   database: process.env.POSTGRES_DATABASE,
