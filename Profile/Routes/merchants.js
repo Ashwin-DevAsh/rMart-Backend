d6817b1c170ba55e7d0c080023fa42f1faeaf94b
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-10 01:49:02 +0530

    order id

--- Profile/Routes/merchants.js
+++ Profile/Routes/merchants.js
@@ -3,8 +3,11 @@ const app = require("express").Router();
 app.get("/getMerchants", (req, res) => {
   res.send([
     {
+      ownername: "Admin",
+      number: "919840573702",
+      email: "kumar@rajalakshmi.edu.in",
       id: "rbusiness@919840573702",
-      accountname: "rec cafe",
+      accountname: "REC CAFE",
     },
   ]);
 });
