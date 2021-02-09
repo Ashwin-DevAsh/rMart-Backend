00086cfd8f342f198d74f9400d19be85f3b22694
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-09 19:01:21 +0530

    Mart

--- Mart/Services/OrderService.js
+++ Mart/Services/OrderService.js
@@ -26,7 +26,7 @@ module.exports = class OrderService {
             orderdBy,
             transactionTime,
             products,
-            { amount },
+            { amount, paymentStatus: "pending" },
             false,
           ]
         )
