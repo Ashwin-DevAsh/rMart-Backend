388b0f9fe4efe9116f8e07f95c30761c405c7a30
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-10 01:39:36 +0530

    order id

--- Profile/Routes/merchants.js
+++ Profile/Routes/merchants.js
@@ -1,10 +1,12 @@
 const app = require("express").Router();
 
 app.get("/getMerchants", (req, res) => {
-  res.send({
-    id: "rbusiness@919840573702",
-    name: "rec cafe",
-  });
+  res.send([
+    {
+      id: "rbusiness@919840573702",
+      name: "rec cafe",
+    },
+  ]);
 });
 
 module.exports = app;
