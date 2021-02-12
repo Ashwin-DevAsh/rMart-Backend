a41a4f70595e76de24b331f98c936855b602df25
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-12 19:09:24 +0530

    Fix bug

--- Mart/jobs/OrderExpery.js
+++ Mart/jobs/OrderExpery.js
@@ -1,6 +1,6 @@
 var cron = require('node-cron');
  
-var task = cron.schedule('05 19 * * *', () => {
+var task = cron.schedule('15 19 * * *', () => {
   console.log('Order expering...');
   (new (require('../Services/OrderExpery'))).closeOrder()
 },{ 
