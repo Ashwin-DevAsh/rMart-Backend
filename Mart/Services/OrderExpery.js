0de64baf1e5418ad82288a17b092a6e6dd47ab7a
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-12 15:02:34 +0530

    cron

--- Mart/Services/OrderExpery.js
+++ Mart/Services/OrderExpery.js
@@ -4,12 +4,13 @@ const clientDetails = require("../Database/ClientDetails");
 module.exports = class OrderExpery{
     pool = new Pool(clientDetails);
 
-    closeOrder = ()=>{
+    closeOrder = async()=>{
         var postgres = await this.pool.connect();
         try{
             var products = await postgres.query(
                 `update orders set status = 'expaired' WHERE to_timestamp(timestamp, 'MM-DD-YYYY HH24:MI:SS') = TIMESTAMP 'yesterday' return *`
              );
+             console.log(products)
         }catch(error){
             postgres.release();
             console.log(error)
