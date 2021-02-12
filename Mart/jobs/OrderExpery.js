98c50e66e6205c0f4fb20d5c39adf921d7e605a9
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-12 13:29:33 +0530

    cron

--- Mart/jobs/OrderExpery.js
+++ Mart/jobs/OrderExpery.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
  
 var task = cron.schedule('*/2 * * * *', () => {
   console.log('running a task every two minutes');
-},{ scheduled: true,
+},{ 
     timezone: "Asia/Kolkata"});
 
 module.exports = task;
