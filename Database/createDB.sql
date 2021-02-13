6c3550d1afe1e4699a482fd56899e55bfa5de189
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-14 00:53:37 +0530

    sec key

--- Database/createDB.sql
+++ Database/createDB.sql
@@ -19,7 +19,7 @@ create table products(
    category varchar,
    imageUrl varchar,
    availableOn varchar[],
-   isAvaliable boolean;
+   isAvaliable boolean
 );
 
 create table orders(  
@@ -30,7 +30,7 @@ create table orders(
   timestamp varchar,
   products json[],
   paymentMetadata json,
-  isPaymentSuccessful boolean default true;
+  isPaymentSuccessful boolean default true
 );
 
 
@@ -39,11 +39,6 @@ create table otp(
     number varchar primary key,
     otp varchar,
     isVerified Boolean
-);
-
-create table sync(
-    networdID  varchar,
-    martID varchar
 );
 
 create table recoveryOtp(
