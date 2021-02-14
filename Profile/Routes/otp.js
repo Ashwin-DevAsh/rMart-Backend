6006e0b1f159e46b559da5809c0842c9ffe1ee11
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-14 11:42:49 +0530

    Ip Limiter

--- Profile/Routes/otp.js
+++ Profile/Routes/otp.js
@@ -6,7 +6,7 @@ var RateLimit = require('express-rate-limit');
  // only if you're behind a reverse proxy (Heroku, Bluemix, AWS if you use an ELB, custom Nginx setup, etc) 
  
 var limiter = new RateLimit({
-  windowMs: 1*60*1000, // 15 minutes 
+  windowMs: 15*60*1000, // 15 minutes 
   max: 120, // limit each IP to 100 requests per windowMs 
   delayMs: 0 // disable delaying - full speed until the max limit is reached 
 });
