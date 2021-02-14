9700a9e35ad05fd7f35ed3973979a05647f8352d
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-14 12:34:11 +0530

    Increase rate limit

--- Mart/index.js
+++ Mart/index.js
@@ -20,8 +20,8 @@ var helmet = require('helmet')
 const app = express();
 
 var limiter = new RateLimit({
-  windowMs: 15*60*1000, // 15 minutes 
-  max: 120, // limit each IP to 100 requests per windowMs 
+  windowMs: 5*60*1000, // 15 minutes 
+  max: 18000, // limit each IP to 100 requests per windowMs 
   delayMs: 0 // disable delaying - full speed until the max limit is reached 
 });
 
