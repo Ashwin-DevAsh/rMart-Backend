e677eb5d5cb120df9499da5aafb6b4d936220b86
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-16 03:41:44 +0530

    Handel payment

--- Mart/Services/HandelFailedOrdersService.js
+++ Mart/Services/HandelFailedOrdersService.js
@@ -51,6 +51,9 @@ module.exports = class HandelFailedOrderService{
             (ordersDetails.status == "authorized" ||
             ordersDetails.status == "captured") &&
             ordersDetails.amount / 100 == amount / 100;
+          console.log((ordersDetails.status ,"authorized",
+          ordersDetails.status , "captured") &&
+          ordersDetails.amount / 100 , amount / 100)   
           if (isVerified) {
             var data = await postgres.query(
               `update orders set paymentMetadata = $2, isPaymentSuccessful=true where cast(paymentmetadata->>'id' as varchar) = $1 returning *`,
