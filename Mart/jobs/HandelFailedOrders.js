963cc64aebc849a52d92335199e34ca089ed3915
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-16 03:56:26 +0530

    flix bug

--- Mart/jobs/HandelFailedOrders.js
+++ Mart/jobs/HandelFailedOrders.js
@@ -1,7 +1,6 @@
 var cron = require('node-cron');
 
-var task = cron.schedule('*/1 * * * *', () => {
-  console.log('running a task every two minutes');
+var task = cron.schedule('*/30 * * * *', () => {
   (new (require('../Services/HandelFailedOrdersService'))).handleFailedOrders()
 });
 module.exports = task;
