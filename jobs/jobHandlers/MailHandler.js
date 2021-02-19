216a5f935798252c04266d2d85f799bbb8793da9
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-19 19:06:31 +0530

    jobs

--- jobs/jobHandlers/MailHandler.js
+++ jobs/jobHandlers/MailHandler.js
@@ -5,8 +5,8 @@ var task = cron.schedule('*/1 * * * *', () => {
     try{
        axios.post('http://email:8000/sendMail',{
          subject:"Hurry up! The sale for tomorrow's meal closing in an hour!",
-         body:`<p>Hey!
-               The order requests for tomorrow's meal is closing in an hour! Don't miss tomorrow's special recipes from the restaurants at our campus! Grab your phone and order now from our rMart app right away!<br>
+         body:`<p>Hey!<br><br>
+               The order requests for tomorrow's meal is closing in an hour! Don't miss tomorrow's special recipes from the restaurants at our campus! Grab your phone and order now from our rMart app right away!<br><br>
 
                The sale will be open till 9:00 PM. Hurry up!<br>
                 
