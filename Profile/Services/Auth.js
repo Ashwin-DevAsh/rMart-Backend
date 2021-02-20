86edf95256aac243052c989e52455ae3f8bc223a
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-20 18:42:45 +0530

    New Key

--- Profile/Services/Auth.js
+++ Profile/Services/Auth.js
@@ -5,7 +5,7 @@ module.exports = class Auth {
   isKeyAuth = async (req, res, next) => {
     try {
       console.log("key ver");
-      var isVerified = process.env.PRIVATE_KEY == req.get("key");
+      var isVerified = process.env.PRIVATE_KEY == req.get("key") || process.env.MART_SERVER_KEY==req.get("key");
       console.log( process.env.PRIVATE_KEY ," ", req.get("key"))
       if (isVerified) {
         next();
