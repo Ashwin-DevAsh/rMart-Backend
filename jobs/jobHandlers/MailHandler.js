3c32bc1c649ac923e1212fc1dee15c1fc325d95b
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-21 12:38:19 +0530

    fix email

--- jobs/jobHandlers/MailHandler.js
+++ jobs/jobHandlers/MailHandler.js
@@ -3,7 +3,15 @@ var axios = require('axios');
 
 var task1 = cron.schedule('00 20 * * *', () => {
     try{
-       axios.post('http://email:8000/sendMail',{
+
+      var currentTime = new Date();
+      var currentOffset = currentTime.getTimezoneOffset();
+      var ISTOffset = 330; 
+      var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+      var day = ISTTime.getDay()
+
+      if(day!=6)
+      axios.post('http://email:8000/sendMail',{
          subject:"Hurry up! The sale for tomorrow's meal closing in an hour!",
          body:`<p>Hey!<br><br>
                The order requests for tomorrow's meal is closing in an hour! Don't miss tomorrow's special recipes from the restaurants at our campus! Grab your phone and order now from our rMart app right away!<br><br>
@@ -29,7 +37,15 @@ var task1 = cron.schedule('00 20 * * *', () => {
 
 var task2 = cron.schedule('00 12 * * *', () => {
   try{
-     axios.post('http://email:8000/sendMail',{
+
+    var currentTime = new Date();
+    var currentOffset = currentTime.getTimezoneOffset();
+    var ISTOffset = 330; 
+    var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+    var day = ISTTime.getDay()
+
+    if(day!=6)
+    axios.post('http://email:8000/sendMail',{
        subject:"Open now! Sale for tomorrow's meal is live now!",
        body:`<p>Hey!<br><br>
        The sale for tomorrow's meal is open now! Order safe and hygiene meals  from restaurants at our campus.<br><br>
