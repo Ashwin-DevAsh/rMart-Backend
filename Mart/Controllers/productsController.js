2cd26489fa6ae68824051ece65ab7b8324ccf8f7
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-27 00:46:39 +0530

    fix

--- Mart/Controllers/productsController.js
+++ Mart/Controllers/productsController.js
@@ -68,7 +68,7 @@ class ProductsController {
       !imageUrl ||
       !availableOn ||
       !isavailable ||
-      !discount
+      !discount!=undefined
     ) {
       res.send({ message: "invalid body" });
     } else {
@@ -120,7 +120,7 @@ class ProductsController {
       !imageUrl ||
       !availableOn ||
       !isavailable ||
-      !discount
+      !discount!=undefined
     ) {
       res.send({ message: "invalid body" });
     } else {
