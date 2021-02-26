b674c0699cc48e0dabb0e59559abd8dfa6697c27
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-02-27 00:24:15 +0530

    fix

--- Mart/Services/Database.js
+++ Mart/Services/Database.js
@@ -77,6 +77,7 @@ module.exports = class Database {
         ]
       );
       postgres.release();
+      console.log("Update successfully")
       return true;
     } catch (e) {
       postgres.release();
@@ -129,7 +130,6 @@ module.exports = class Database {
         ]
       );
       postgres.release();
-      console.log("Update successfully")
       return true;
     } catch (e) {
       postgres.release();
