c7d830a89a1d75711e5248f7077152f301c99332
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-06 14:47:48 +0530

    ordered expairy

--- jobs/jobHandlers/OrderExpery.js
+++ jobs/jobHandlers/OrderExpery.js
@@ -2,10 +2,19 @@ var cron = require('node-cron');
  
 var task = cron.schedule('30 15 * * *', () => {
   console.log('Order expering...');
-  (new (require('../Services/OrderExpery'))).closeOrder()
+  var currentTime = new Date();
+  var currentOffset = currentTime.getTimezoneOffset();
+  var ISTOffset = 330; 
+  var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
+  var day = ISTTime.getDay()
+
+  if(day!=0){
+    (new (require('../Services/OrderExpery'))).closeOrder()
+  }
 },{ 
     scheduled: true,
-    timezone: "Asia/Kolkata"}
+    timezone: "Asia/Kolkata"
+  }
 );
 
 module.exports = task;
