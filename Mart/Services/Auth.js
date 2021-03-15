bec71db38c6f4ad912d1615e4e663487aef6f2a6
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-16 01:12:20 +0530

    Mail

--- Mart/Services/Auth.js
+++ Mart/Services/Auth.js
@@ -66,18 +66,16 @@ module.exports = class Auth {
 
   isMartOpen = async (req, res, next) => {
 
-    next();
+    var currentTime = new Date();
+    var currentOffset = currentTime.getTimezoneOffset();
+    var ISTOffset = 330; 
+    var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
 
-    // var currentTime = new Date();
-    // var currentOffset = currentTime.getTimezoneOffset();
-    // var ISTOffset = 330; 
-    // var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
-
-    // var currentHour = ISTTime.getHours()
-    // if (currentHour >= 12 && currentHour < 23) {
-    //   next();
-    // } else {
-    //   res.send({ message: "closed" });
-    // }
+    var currentHour = ISTTime.getHours()
+    if (currentHour >= 12 && currentHour < 23) {
+      next();
+    } else {
+      res.send({ message: "closed" });
+    }
   };
 };
