2153b4d57293a2f4c0ca8c6087e03503fc1567c2
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-15 23:41:30 +0530

    Mail

--- Mart/Controllers/productPictureController.js
+++ Mart/Controllers/productPictureController.js
@@ -22,7 +22,7 @@ module.exports = class ProductsPictureController {
 
   getQrImages = (req, res) => {
     var imageName = req.params.imageName;
-    res.sendFile("../QrImages/" + imageName);
+    res.sendFile( imageName,{root:"../QrImages/"});
   };
 
   addProductPicture = (req, res) => {
