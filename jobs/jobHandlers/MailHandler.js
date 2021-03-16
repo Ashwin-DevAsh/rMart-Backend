568705033f929026756ab68d782f5d3b6faeb2ce
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-16 22:52:16 +0530

    Remove time limit

--- jobs/jobHandlers/MailHandler.js
+++ jobs/jobHandlers/MailHandler.js
@@ -11,11 +11,11 @@ var task1 = cron.schedule('00 22 * * *', () => {
       var day = ISTTime.getDay()
 
       if(day!=6){
-          axios.post('http://email:8000/sendNotification',{
-            title:"Closing soon!",
-            subtitle:"The order requests for tomorrow's sale is closing in an hour!",
-            topic:'rMart'
-          })
+          // axios.post('http://email:8000/sendNotification',{
+          //   title:"Closing soon!",
+          //   subtitle:"The order requests for tomorrow's sale is closing in an hour!",
+          //   topic:'rMart'
+          // })
       }
        console.log("sended email")
     }catch(e){
@@ -39,30 +39,30 @@ var task2 = cron.schedule('00 12 * * *', () => {
     var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
     var day = ISTTime.getDay()
 
-    if(day!=6){
-      axios.post('http://email:8000/sendMail',{
-        subject:"Open now! Sale for tomorrow's meal is live now!",
-        body:`<p>Hey!<br><br>
-        The sale for tomorrow's meal is open now! Order safe and hygiene meals  from restaurants at our campus.<br><br>
+    // if(day!=6){
+    //   axios.post('http://email:8000/sendMail',{
+    //     subject:"Open now! Sale for tomorrow's meal is live now!",
+    //     body:`<p>Hey!<br><br>
+    //     The sale for tomorrow's meal is open now! Order safe and hygiene meals  from restaurants at our campus.<br><br>
         
-        The sale will be open till 11:00 PM tonight.<br><br>
+    //     The sale will be open till 11:00 PM tonight.<br><br>
         
-        Feel free to drop your suggestion/queries /bug report @ rmart.support@rajalakshmi.edu.in<br><br>
+    //     Feel free to drop your suggestion/queries /bug report @ rmart.support@rajalakshmi.edu.in<br><br>
 
-        Download app from https://play.google.com/store/apps/details?id=com.DevAsh.RMart <br><br>
+    //     Download app from https://play.google.com/store/apps/details?id=com.DevAsh.RMart <br><br>
         
-        Regards,<br>
-        rMart Team</p>`,
-        to:'students@rajalakshmi.edu.in'
-      })
-    }
+    //     Regards,<br>
+    //     rMart Team</p>`,
+    //     to:'students@rajalakshmi.edu.in'
+    //   })
+    // }
 
 
-    axios.post('http://email:8000/sendNotification',{
-      title:"Open Now!",
-      subtitle:"The sale for tomorrow's meal is open now!",
-      topic:'rMart'
-    })
+    // axios.post('http://email:8000/sendNotification',{
+    //   title:"Open Now!",
+    //   subtitle:"The sale for tomorrow's meal is open now!",
+    //   topic:'rMart'
+    // })
      console.log("sended email")
   }catch(e){
     console.log(e)
@@ -84,13 +84,13 @@ var task3 = cron.schedule('00 18 * * *', () => {
     var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
     var day = ISTTime.getDay()
 
-    if(day!=6){
-      axios.post('http://email:8000/sendNotification',{
-        title:"You there!",
-        subtitle:"Haven't ordered foods for tommorow yet? Grab your phone right away and order now!",
-        topic:'rMart'
-      })
-    }
+    // if(day!=6){
+    //   axios.post('http://email:8000/sendNotification',{
+    //     title:"You there!",
+    //     subtitle:"Haven't ordered foods for tommorow yet? Grab your phone right away and order now!",
+    //     topic:'rMart'
+    //   })
+    // }
      console.log("sended email")
   }catch(e){
     console.log(e)
@@ -111,13 +111,13 @@ var task4 = cron.schedule('50 22 * * *', () => {
     var ISTTime = new Date(currentTime.getTime() + (ISTOffset + currentOffset)*60000);
     var day = ISTTime.getDay()
 
-    if(day!=6){
-        axios.post('http://email:8000/sendNotification',{
-          title:"Hurry up!",
-          subtitle:"The order requests are closing soon in few minutes! Grab your phone and order now!",
-          topic:'rMart'
-        })
-    }
+    // if(day!=6){
+    //     axios.post('http://email:8000/sendNotification',{
+    //       title:"Hurry up!",
+    //       subtitle:"The order requests are closing soon in few minutes! Grab your phone and order now!",
+    //       topic:'rMart'
+    //     })
+    // }
      console.log("sended email")
   }catch(e){
     console.log(e)
