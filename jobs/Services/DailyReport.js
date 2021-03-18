a87d521d4be292d3b89636fb154134b0ca81774b
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-18 18:19:49 +0530

    Daily report

---
+++ jobs/Services/DailyReport.js
@@ -0,0 +1,24 @@
+const { Pool } = require("pg");
+const clientDetails = require("../Database/ClientDetails");
+
+module.exports = class DailyReport{
+    pool = new Pool(clientDetails);
+
+    getTodayOrders = async()=>{
+        var postgres = await this.pool.connect();
+        try{
+             await postgres.query(`set timezone TO 'Asia/Kolkata'`);
+             var todayOrders = await postgres.query(
+                `select * from orders WHERE to_timestamp(timestamp, 'MM-DD-YYYY HH24:MI:SS') >= TIMESTAMP 'today' and ispaymentsuccessful is true;`
+             );
+             console.log(todayOrders.rows)
+             postgres.release();
+             return todayOrders.rows
+
+        }catch(error){
+            postgres.release();
+            console.log(error)
+            return [];
+        }
+    }
+} 
--- jobs/jobHandlers/MailHandler.js
+++ jobs/jobHandlers/MailHandler.js
@@ -1,5 +1,6 @@
 var cron = require('node-cron');
 var axios = require('axios');
+var dailyReport = new (require('../Services/DailyReport'))();
 
 var task1 = cron.schedule('00 07 * * *', () => {
     try{
@@ -58,6 +59,86 @@ timezone: "Asia/Kolkata"
 );
 
 
+var task3 = cron.schedule('23 18 * * *', () => {
+  console.log('task 3 executing...')
+  try{
+
+    var todayOrders = dailyReport.getTodayOrders()
+
+    var totalOrders = 0;
+    var pendingOredrs = 0;
+    var deliveredOrders = 0;
+    var productString = ``
+    var productMap = {}
+
+    for(var i in todayOrders){
+      var order = todayOrders[i]
+      totalOrders+=1
+      if(order.status == 'pending'){
+          pendingOredrs+=1
+          continue;
+      }else{
+          deliveredOrders+=1
+      }
+
+      for(var i in order){
+        var productID = order[i].productID
+        try{
+          productMap[productID].productName =  order[i].product.productName;
+          productMap[productID].count += order[i].count
+          productMap[productID].totalPrice += order[i].totalPrice
+        }catch(e){
+          productMap[productID] = {
+            productName : product.productName,
+            count : count,
+            totalPrice:totalPrice
+          }
+        }
+      }
+    }
+
+    for( var i in productMap){
+      var product = productMap[i]
+
+      productString += `<tr> 
+        <td style=" border: 1px solid #dddddd; padding: 8px;text-align: left;" >${product.productName}</td>
+        <td style=" border: 1px solid #dddddd; padding: 8px;text-align: left;" >${product.count}</td>
+        <td style=" border: 1px solid #dddddd; padding: 8px;text-align: left;" >${product.totalPrice} Rs</td>
+      </tr>` 
+    }
 
 
-module.exports = {task1,task2};
+    axios.post('http://email:8000/sendMail',{
+      subject:"New Order",
+      body:`<p>
+              Total Orders         ${totalOrders} <br/>
+              Pending Orders       ${pendingOredrs} <br/>
+              Delivered Orders     ${deliveredOrders} <br/><br/><br/>
+              <table style="width:100%;" >
+                <tr>
+                  <th style=" border: 1px solid #dddddd; padding: 8px;text-align: left;" >Product</th>
+                  <th style=" border: 1px solid #dddddd; padding: 8px;text-align: left;">Count</th>
+                  <th style=" border: 1px solid #dddddd; padding: 8px;text-align: left;" >Amount</th>
+                </tr>
+                ${productString}
+              </table>
+           </p>`,
+     to:'2017ashwin@gmail.com',
+    })
+
+     console.log("sended email")
+  }catch(e){
+    console.log(e)
+  }
+},
+{ 
+scheduled: true,
+timezone: "Asia/Kolkata"
+}
+);
+
+
+
+
+
+module.exports = {task1,task2,task3};
