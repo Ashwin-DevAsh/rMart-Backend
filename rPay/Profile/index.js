327924ed1a1f530acef504d9251e04e727b6ccb0
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-19 19:50:22 +0530

    nginx

--- rPay/Profile/index.js
+++ rPay/Profile/index.js
@@ -3,13 +3,13 @@ require("dotenv").config({ path: "./env/.env" });
 const express = require("express");
 
 const bodyParser = require("body-parser");
-// const authEndPoint = require("./Routes/Auth");
-// const userEndPoint = require("./Routes/User");
-// const merchantEndPoint = require("./Routes/Merchants");
-// const recovery = require("./Routes/Recovery");
-// const bankAccounts = require("./Routes/BankAccounts");
-// const uploadPictures = require("./Routes/UploadPictures");
-// const init = require("./Routes/init");
+const authEndPoint = require("./Routes/Auth");
+const userEndPoint = require("./Routes/User");
+const merchantEndPoint = require("./Routes/Merchants");
+const recovery = require("./Routes/Recovery");
+const bankAccounts = require("./Routes/BankAccounts");
+const uploadPictures = require("./Routes/UploadPictures");
+const init = require("./Routes/init");
 
 process.env.TZ = "Asia/Kolkata";
 const app = express();
@@ -30,13 +30,13 @@ app.get("/", (req, res) => {
   res.status(200).json({ Message: "Welcome to rec wallet" });
 });
 
-// app.use(userEndPoint);
-// app.use(authEndPoint);
-// app.use(merchantEndPoint);
-// app.use(recovery);
-// app.use(bankAccounts);
-// app.use(uploadPictures);
-// app.use(init);
+app.use(userEndPoint);
+app.use(authEndPoint);
+app.use(merchantEndPoint);
+app.use(recovery);
+app.use(bankAccounts);
+app.use(uploadPictures);
+app.use(init);
 
 app.listen(8000, () => {
   console.log("connecte at port 8000");
