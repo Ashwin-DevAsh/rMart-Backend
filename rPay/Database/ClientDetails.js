c05a39260dc9e4da8e613db66b21fd4620e1b4db
Ashwin-DevAsh <2022ashwin@gmail.com>
2021-03-19 14:36:25 +0530

    nginx

--- rPay/Database/ClientDetails.js
+++ rPay/Database/ClientDetails.js
@@ -1,5 +1,5 @@
 module.exports = {
-  host: process.env.POSTGRES_URL,
+  host: process.env.RPAY_POSTGRES_URL,
   port: 5432,
   user: process.env.RPAY_POSTGRES_USER,
   password: process.env.RPAY_POSTGRES_PASSWORD,
