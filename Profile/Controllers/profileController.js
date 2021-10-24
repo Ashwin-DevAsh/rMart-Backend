a0818f6cd1e8c48889caa57de4717a9e4986799f
Ashwin-devAsh <2017ashwin@gmail.com>
2021-10-24 22:18:25 +0530

    Added profile

--- Profile/Controllers/profileController.js
+++ Profile/Controllers/profileController.js
@@ -9,7 +9,7 @@ module.exports = class ProfileController {
         res.send({ message: "invalid body" });
         return;
       }
-      var balance = this.databaseService.getBalance(id)
+      var balance = await this.databaseService.getBalance(id)
       console.log(balance)
       res.send({"message":"done",balance})
   }
