a367f8c032f02a8f033ebcf03f16e9cbda45e3ae
Ashwin-devAsh <2017ashwin@gmail.com>
2021-11-02 13:31:56 +0530

    Added cash back offer

--- jobs/jobHandlers/WeeklyCashbackOffer.js
+++ jobs/jobHandlers/WeeklyCashbackOffer.js
@@ -2,7 +2,7 @@ var cron = require('node-cron');
 var offerService = new (require('../Services/OfferService'))();
 
  
-var task = cron.schedule('25 13 * * *', async () => {
+var task = cron.schedule('0 0 * * Sun', async () => {
     console.log("initing cash back offer")
     await offerService.initCashbackOffer()
 },{ 
