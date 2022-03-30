a5cb04530880451452f80c892f66c3ce41666c1f
Ashwin-DevAsh <2017ashwin@gmail.com>
2022-03-31 00:42:46 +0530

    env

--- productUploader.py
+++ productUploader.py
@@ -2,7 +2,7 @@ import requests
 from products import *
 true = True
 def uploadProduct(product):
-    url = "https://mart.rajalakshmimart.com/addProducts"
+    url = "https://mart.rajalakshmimart.com/updateProduct"
     response = requests.post(
        url=url,
        headers={'key':'2021@merchant/server/key|{www.devash.tech}~merChAnT'},
